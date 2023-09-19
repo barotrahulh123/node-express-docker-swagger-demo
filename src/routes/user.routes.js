e5d7123a898170fa2039c6ae2921430771fcaf62
rahul.b <rahul.b@simformsolutions.com>
2023-09-19 10:08:22 +0530

    build docker image solve issue

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -55,8 +55,50 @@ const userRoutes = Router();
  *                 createdAt: "2023-09-17T06:14:21.427Z"
  *                 updatedAt: "2023-09-17T06:14:21.427Z"
  */
-userRoutes.post("/user", userController.add);
+
 userRoutes.get("/user", userController.get);
+/**
+ * @swagger
+ * /user:
+ *   post:
+ *     summary: Create a new user
+ *     description: Create a new user record in the database.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: The name of the user.
+ *               email:
+ *                 type: string
+ *                 format: email
+ *                 description: The email address of the user.
+ *               username:
+ *                 type: string
+ *                 description: The username of the user.
+ *               password:
+ *                 type: string
+ *                 description: The password of the user.
+ *               stateId:
+ *                 type: integer
+ *                 description: The ID of the state associated with the user.
+ *           example:
+ *             name: Plantix
+ *             email: admin@blueanalytics.com
+ *             username: blueanalytics
+ *             password: blueanalytics@123
+ *             stateId: 1
+ *     responses:
+ *       '201':
+ *         description: User created successfully
+ *       '400':
+ *         description: Bad request
+ */
+userRoutes.post("/user", userController.add);
 userRoutes.get("/user/:id", userController.find);
 
 export { userRoutes };
